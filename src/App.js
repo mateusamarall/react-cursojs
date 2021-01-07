b01eee0f878a8c64bee899bd30a7321b65bfa798
mateusamarall <mateus.amaral018@gmail.com>
2021-01-07 18:38:53 -0300

    adicionado eslint na aplicação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import React from "react";
+import React from 'react';
 import './styles.css';
 
 function App() {
-
   return (
     <p>e eu pago em cabeça de gado - wellison</p>
-  )
-
-
+  );
 }
 
 export default App;
